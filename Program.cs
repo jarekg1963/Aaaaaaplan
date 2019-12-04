f4d715b35f825ecbcf8a8700d15e35a6250d9d79
Jaroslaw J. G. Granat <jaroslaw.granat@tpvaoc.com>
2019-12-04 14:27:18 +0100

    Rozdzielenie zmiennych po klasach

--- Program.cs
+++ Program.cs
@@ -10,32 +10,29 @@ namespace prservice
     {
         static void Main(string[] args)
         {
-            // string katalogRoboczy = @"c:\testy\SQL\Anaplan\";
-            // string dzien = (DateTime.Today.Day < 10) ? "0" + DateTime.Today.Day.ToString() : DateTime.Today.Day.ToString();
-            // string mc = (DateTime.Today.Month < 10) ? "0" + DateTime.Today.Month.ToString() : DateTime.Today.Month.ToString();
-            // string StrData = dzien + mc + DateTime.Today.Year.ToString();
-            // string pathPlik = katalogRoboczy + "AnaplanToEflow" + StrData + ".csv";
+
             Generuj g = new Generuj();
             g.GenerujPlik();
-            SendFileToServer.Send( @"c:\testy\SQL\Anaplan\AnaplanToEflow04122019.csv");
-
-
-
+            SendFileToServer s = new SendFileToServer();
+            s.Send();
 
         }
 
-       public class Generuj
+        class Generuj
         {
+
             public void GenerujPlik()
             {
-                string katalogRoboczy = @"c:\testy\SQL\Anaplan\AnaplanToEflow04122019.csv";
+                string katalogRoboczy = @"c:\testy\SQL\Anaplan\";
                 string dzien = (DateTime.Today.Day < 10) ? "0" + DateTime.Today.Day.ToString() : DateTime.Today.Day.ToString();
                 string mc = (DateTime.Today.Month < 10) ? "0" + DateTime.Today.Month.ToString() : DateTime.Today.Month.ToString();
                 string StrData = dzien + mc + DateTime.Today.Year.ToString();
                 string pathPlik = katalogRoboczy + "AnaplanToEflow" + StrData + ".csv";
 
+                string plikSql = katalogRoboczy + "sqlcomenda.txt";
 
-                string polecenieSQL = System.IO.File.ReadAllText(@"c:\testy\SQL\sqlcomenda.txt");
+
+                string polecenieSQL = System.IO.File.ReadAllText(plikSql);
                 // Display the file contents to the console. Variable text is a string.
                 System.Console.WriteLine("Contents of WriteText.txt = {0}", polecenieSQL);
 
@@ -62,7 +59,7 @@ namespace prservice
             }
         }
 
-        public class SendFileToServer
+        class SendFileToServer
         {
             // Enter your host name or IP here
             private static string host = "37.74.15.90";
@@ -70,21 +67,28 @@ namespace prservice
             private static string username = "sap.anaplan";
             // Enter your sftp password here
             private static string password = "Kgqd1z6K";
-            public static int Send(string fileName)
+
+            static string katalogRoboczy = @"c:\testy\SQL\Anaplan\";
+            static string dzien = (DateTime.Today.Day < 10) ? "0" + DateTime.Today.Day.ToString() : DateTime.Today.Day.ToString();
+            static string mc = (DateTime.Today.Month < 10) ? "0" + DateTime.Today.Month.ToString() : DateTime.Today.Month.ToString();
+            static string StrData = dzien + mc + DateTime.Today.Year.ToString();
+            string pathPlik = katalogRoboczy + "AnaplanToEflow" + StrData + ".csv";
+            string plik = "AnaplanToEflow" + StrData + ".csv";
+            public void Send()
             {
                 var connectionInfo = new ConnectionInfo(host, "sftp", new PasswordAuthenticationMethod(username, password));
                 // Upload File
                 using (var sftp = new SftpClient(connectionInfo))
                 {
                     sftp.Connect();
-                     sftp.ChangeDirectory("PR Report");
-                    using (var uplfileStream = System.IO.File.OpenRead(fileName))
+                    sftp.ChangeDirectory("PR Report");
+                    using (var uplfileStream = System.IO.File.OpenRead(pathPlik))
                     {
-                        sftp.UploadFile(uplfileStream, "AnaplanToEflow04122019.csv");
+                        sftp.UploadFile(uplfileStream, plik);
                     }
                     sftp.Disconnect();
                 }
-                return 0;
+
             }
         }
     }
