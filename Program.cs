a7407980b019a964e42b8517137d0c34ff8c9878
Jaroslaw J. G. Granat <jaroslaw.granat@tpvaoc.com>
2019-12-04 10:08:29 +0100

    TworzeniePliku

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data.SqlClient;
+using System.IO;
 
 namespace prservice
 {
@@ -7,31 +8,50 @@ namespace prservice
     {
         static void Main(string[] args)
         {
-           
+            Generuj g = new Generuj();
 
-            string polecenieSQL = System.IO.File.ReadAllText(@"c:\testy\SQL\sqlcomenda.txt");
-
-            // Display the file contents to the console. Variable text is a string.
-            System.Console.WriteLine("Contents of WriteText.txt = {0}", polecenieSQL);
+            g.GenerujPlik();
 
 
+        }
 
-
-            // var connectionString = @"Server=DESKTOP-M9PRPPC\MSSQLSERVER01;Database=Derogation_System;Trusted_Connection=True;";
-            var connectionString = @"Data Source=172.26.60.102;Initial Catalog=MRO;User =webdb.admin; Password = Webdb.Admin ";
-            using (var client = new SqlConnection(connectionString))
+        class Generuj
+        {
+            public void GenerujPlik()
             {
-                client.Open();
-                SqlCommand cmd = new SqlCommand(polecenieSQL , client);
+                string katalogRoboczy = @"c:\testy\SQL\Anaplan\";
 
-                SqlDataReader reader = cmd.ExecuteReader();
-                while (reader.Read())
+                string polecenieSQL = System.IO.File.ReadAllText(@"c:\testy\SQL\sqlcomenda.txt");
+                string dzien = (DateTime.Today.Day < 10) ? "0" + DateTime.Today.Day.ToString() : DateTime.Today.Day.ToString();
+                string mc = (DateTime.Today.Month < 10) ? "0" + DateTime.Today.Month.ToString() : DateTime.Today.Month.ToString();
+
+                // Display the file contents to the console. Variable text is a string.
+                System.Console.WriteLine("Contents of WriteText.txt = {0}", polecenieSQL);
+
+                // var connectionString = @"Server=DESKTOP-M9PRPPC\MSSQLSERVER01;Database=Derogation_System;Trusted_Connection=True;";
+                var connectionString = @"Data Source=172.26.60.102;Initial Catalog=MRO;User =webdb.admin; Password = Webdb.Admin ";
+                using (var client = new SqlConnection(connectionString))
                 {
-                    Console.WriteLine("\t{0}",
-                        reader[0]);
+                    client.Open();
+                    SqlCommand cmd = new SqlCommand(polecenieSQL, client);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    string StrData = dzien + mc + DateTime.Today.Year.ToString();
+                    string pathPlik = katalogRoboczy + "AnaplanToEflow" + StrData + ".csv";
+                    StreamWriter sw = File.CreateText(pathPlik);
+
+                    while (reader.Read())
+                    {
+                        sw.WriteLine(reader[0].ToString());
+
+                        Console.WriteLine("\t{0}",
+                            reader[0]);
+                    }
+                    reader.Close();
+                    sw.Dispose();
                 }
-                reader.Close();
             }
+
         }
     }
 }
